525ded6e8595635a082cd72e2f3d7334cbeeedfc
hamada <mariam.hamada.2020@gmail.com>
2024-02-11 09:31:42 +0200

    37

--- app/Review.php
+++ app/Review.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function scopeSelection($query)
+    {
+        return $query->select(
+        	'id',
+        	'rate',
+            'comment',
+            'date',
+            'time'
+        );
+    }
 }
