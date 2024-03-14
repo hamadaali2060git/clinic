8b66fc561d841747760afa3d83d84bec3ab59674
hamada <mariam.hamada.2020@gmail.com>
2024-03-14 11:53:14 +0200

    8

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -275,11 +275,11 @@ class HomeController extends Controller
     {
         $user = User::findOrFail($request->user_id);
         $diagnosis = Diagnos::with('categories')->where('user_id',$request->user_id)->orderBy('id', 'DESC')->take(10)->get();
-        $records= Record::where("user_id" , $request->user_id)->get();
+        $records= Record::where("user_id" , $request->user_id)->count();
         $data  =[
             'user'=>new UserResource($user),
             'diagnosis'=>$diagnosis
-            'records_total'=>count($records),
+            'records_total'=>$records,
         ];
         return $this -> returnDataa('data',$data,'');
     }
