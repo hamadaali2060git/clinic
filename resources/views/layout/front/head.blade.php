1eca7dcb23e2b7931a76374b41f54ce0e29be723
Hamada <hamada@Hamadas-MBP.local>
2025-05-10 11:12:37 +0300

    edit tabe title

--- resources/views/layout/front/head.blade.php
+++ resources/views/layout/front/head.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
 <title>drehabcenter</title>
 <!-- Favicons -->
-<link type="image/x-icon" href="{{asset('assets/img/favicon.png')}}" rel="icon">
+<link type="image/x-icon" href="{{asset('img/settings/'.$contact->logo) }}" rel="icon">
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
 
